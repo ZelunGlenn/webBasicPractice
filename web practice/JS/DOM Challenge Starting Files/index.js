938ac907e94f56c112fde024c8e6a61bce96b384
Zelun Zhang <121286574+ZelunGlenn@users.noreply.github.com>
2024-08-03 15:07:49 +0930

    rock paper random game done

--- web practice/JS/DOM Challenge Starting Files/index.js
+++ web practice/JS/DOM Challenge Starting Files/index.js
@@ -1,1 +1,10 @@
 document.querySelector('ul').lastElementChild.innerHTML = 'Glenn'
+
+
+
+const changeSize = () => {
+  document.querySelector('h1').classList.toggle("huge")
+}
+
+
+document.querySelector(' li a ').setAttribute('href', "https://www.baidu.com")
---
+++ web practice/RockPaperTool/index.js
@@ -0,0 +1,16 @@
+var options = ["./images/rock.webp","./images/hand.png"]
+const result = () => {
+  var player1Got = Math.round(Math.random())
+  var player2Got = 0;
+  if ( player1Got === 0 ) {
+    player2Got = 1;
+  }
+  document.querySelector('.player1Hand').innerHTML = `<img src="${options[player1Got]}" alt="">`;
+  document.querySelector('.player2Hand').innerHTML = `<img src="${options[player2Got]}" alt="">`;
+  if (player1Got === 0) {
+    document.querySelector('.winner').textContent = "Winner is Player 2"
+  } else {
+    document.querySelector('.winner').textContent = "Winner is Player 1"
+  }
+  
+}
