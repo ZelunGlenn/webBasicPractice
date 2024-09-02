5c82828030009d97a8b6690a83d3098ddbf3e423
Zelun Zhang <121286574+ZelunGlenn@users.noreply.github.com>
2024-09-02 21:44:52 +0930

    add animation to the card button when user pressed

--- web practice/AppleShowcaseDemo/src/index.js
+++ web practice/AppleShowcaseDemo/src/index.js
@@ -3,34 +3,45 @@ const audio0 = new Audio(`./sounds/music0.mp3`)
 const audio1 = new Audio(`./sounds/music1.mp3`)
 
 const buttons = document.querySelectorAll('button');
+
+
+const videos = document.querySelectorAll('video');
+videos.forEach((video) => {
+  video.pause()
+})
+
+
 buttons.forEach((button, index) => {
-  const audio = new Audio(`./sounds/music${index}.mp3`);
+  const audio = new Audio(`./sounds/music${index}.mp3`)
 
   // Create a Web Audio API context
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  const track = audioContext.createMediaElementSource(audio);
-  const gainNode = audioContext.createGain();
-  track.connect(gainNode).connect(audioContext.destination);
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+  const track = audioContext.createMediaElementSource(audio)
+  const gainNode = audioContext.createGain()
+  track.connect(gainNode).connect(audioContext.destination)
 
-  button.addEventListener('mouseenter', () => {
+  button.addEventListener('mouseenter', (e) => {
+    const titleDiv = e.currentTarget.querySelector('.titles');
+    
+    playVideo(titleDiv.innerText)
     audioContext.resume().then(() => {
-    gainNode.gain.setValueAtTime(1, audioContext.currentTime); // Reset volume to 1
-    audio.play();
-    });
-  });
+    gainNode.gain.setValueAtTime(1, audioContext.currentTime) // Reset volume to 1
+    audio.play()
+    })
+  })
 
   button.addEventListener('mouseleave', () => {
-    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 1);
+    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 1)
 
+    pauseVideo()
     // Pause the audio after the fade-out is complete
     setTimeout(() => {
       audio.pause();
-      audio.currentTime = 0; // Reset the audio to the beginning
-      gainNode.gain.setValueAtTime(1, audioContext.currentTime); // Reset volume to 1 for next play
-    }, 1000);
-  });
-});
-console.log("hehe")
+      audio.currentTime = 0 // Reset the audio to the beginning
+      gainNode.gain.setValueAtTime(1, audioContext.currentTime) // Reset volume to 1 for next play
+    }, 1000)
+  })
+})
 
 document.addEventListener('keydown', (event) => {
 
@@ -60,6 +71,22 @@ document.addEventListener('keydown', (event) => {
   }
 
 })
+
+const playVideo = (text) => {
+  console.log(text)
+  if (text === "AirPods Pro") {
+    videos[0].play()
+    videos[1].pause()
+  } else {
+    videos[1].play()
+    videos[0].pause()
+  }
+}
+const pauseVideo = () => {
+
+  videos[1].pause()
+  videos[0].pause()
+}
 
 
 // const playMusic = () => {
