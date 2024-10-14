873fcb5d071f37531652240a3c13476b77b9146b
Zelun Zhang <121286574+ZelunGlenn@users.noreply.github.com>
2024-10-14 19:33:53 +1030

    axios request for outsourse API

--- web practice/APIPractice/5.3 Axios/index.js
+++ web practice/APIPractice/5.3 Axios/index.js
@@ -25,8 +25,32 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
-
+  let filter = ""
+  if (req.body["type"] !== '') {
+    if (req.body['participants'] !== '') {
+      filter = "type=" + req.body["type"] + "&participants=" + req.body["participants"];
+    } else {
+      filter = "type=" + req.body["type"];
+    }
+  } else {
+    if (req.body["participants"] !== '') {
+      filter = "participants=" + req.body["participants"];
+    } else {
+      res.render("index.ejs", {
+        error: "No selected participants or type"
+      })
+    }
+  }
+  console.log(filter)
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/filter?" + filter);
+    const result = response.data;
+    console.log(result)
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", { error: error.message });
+  }
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
