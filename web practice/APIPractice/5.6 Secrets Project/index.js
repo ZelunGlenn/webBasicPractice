897006fcadece5c2246a8b3c925c5cc15adaab57
Zelun Zhang <121286574+ZelunGlenn@users.noreply.github.com>
2024-10-15 21:12:04 +1030

    practice axios request

--- web practice/APIPractice/5.6 Secrets Project/index.js
+++ web practice/APIPractice/5.6 Secrets Project/index.js
@@ -1,6 +1,29 @@
 // HINTS:
 // 1. Import express and axios
+import express from 'express'
+import axios from 'axios'
 
+const app = express()
+const port = 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/', async (req, res) => {
+  const rawData = await axios.get("https://secrets-api.appbrewery.com/random")
+  console.log(JSON.stringify(rawData.data))
+  res.render('index.ejs', {
+    // remove " and " at the beginning and end of the JSON.stringify
+    secret: JSON.stringify(rawData.data.secret).replace(/"/g, ""),
+    user: JSON.stringify(rawData.data.username).replace(/"/g, "")
+    
+    
+  })
+})
+
+app.listen(port, () => {
+  console.log(`port running on https://localhost:${port}`)
+})
 // 2. Create an express app and set the port number.
 
 // 3. Use the public folder for static files.
