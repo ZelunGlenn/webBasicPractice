d19cffc39be7a19be67e4ab2dcca9bc8f835dbe6
Zelun Zhang <zelunzhangofficial@gmail.com>
2024-11-11 14:21:15 +1030

    use session, passport, strategy to build a session and cookies for user
    User opens /login to access the login form.
    User submits the form, triggering a POST /login request.
    passport.authenticate with "local" strategy initiates verification.
    passport-local strategy verifies user credentials:
    Queries the database for the user’s email.
    Uses bcrypt.compare to validate the password.
    On success:
    passport.serializeUser stores session data.
    User is redirected to /secrets.
    On accessing /secrets, req.isAuthenticated() allows or denies access based on the session.

--- web practice/Authentication and Security/9.3+Sessions+and+Cookies/index.js
+++ web practice/Authentication and Security/9.3+Sessions+and+Cookies/index.js
@@ -1,20 +1,54 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
+
+// express-session
+//   for let system can use cookies
+// "passport": "^0.7.0",
+//     "passport-local": "^1.0.0",
+//         for different authentication strategies
+//            passport-local: a strategy for passport that uses a username and password for authentication
+
+
+
+
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
 
+dotenv.config();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// cookie setup: 1st step
+app.use(session({
+  secret: "my-secret-key",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    // one day
+    maxAge: 1000 * 60 * 60 * 24,
+  }
+}))
+
+// cookie setup: 2nd step
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const password = process.env.PASSWORD;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "secrets",
-  password: "123456",
+  database: "Auth01",
+  password: password,
   port: 5432,
 });
 db.connect();
@@ -30,6 +64,14 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
+  }
+})
 
 app.post("/register", async (req, res) => {
   const email = req.body.username;
@@ -49,11 +91,19 @@ app.post("/register", async (req, res) => {
           console.error("Error hashing password:", err);
         } else {
           console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
-          res.render("secrets.ejs");
+          const user = result.rows[0];
+
+          // from password package, we're using passport-local-strategy
+          req.login(user, (err) => {
+            if (err) {
+              console.error(err);
+            }
+            res.redirect("/secrets");
+          })
         }
       });
     }
@@ -62,35 +112,50 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const loginPassword = req.body.password;
+app.post("/login", passport.authenticate("local", {
+
+  successRedirect: "/secrets",
+  failureRedirect: "/login"
+}));
+
+passport.use(new Strategy(async function verify(username, password, cb) {
 
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
+      username,
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
       const storedHashedPassword = user.password;
-      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+      bcrypt.compare(password, storedHashedPassword, (err, result) => {
         if (err) {
-          console.error("Error comparing passwords:", err);
+          return cb(err)
         } else {
           if (result) {
-            res.render("secrets.ejs");
+            return cb(null, user)
           } else {
-            res.send("Incorrect Password");
+            return cb("Wrong password", false)
           }
         }
       });
     } else {
-      res.send("User not found");
+      return cb("No user found", false)
     }
   } catch (err) {
-    console.log(err);
+    return cb(err)
   }
-});
+
+
+}))
+
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+})
+
+passport.deserializeUser(function (user, cb) {
+  cb(null, user);
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
