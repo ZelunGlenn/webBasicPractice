e99c0b939d31608c171fda93fcd011fa237ca019
Zelun Zhang <zelunzhangofficial@gmail.com>
2024-11-20 09:59:26 +1030

    practice on how to do dynamic content and how to pass info to a react tag
    Dynamic content: { X }
    
    pass to react function.
    < X attri ... > or < X {...object[0/1/2/3]} >
    
    in function:
    function (props) or function ({attr, attr, attr}) or function ({...props})
    
    import array or object from a file:
    
    import {XX} from 'file direction'

--- web practice/React/Components, JSX, Props, States/01-starting-project/src/App.jsx
+++ web practice/React/Components, JSX, Props, States/01-starting-project/src/App.jsx
@@ -1,18 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import heroImage from './assets/react-core-concepts.png';
+import reUseSectionImg1 from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+
+const options = ['basic', 'intermediate', 'advanced'];
+
+const generationNumber = (max) => {  
+  // generate a random numebr that change every 2 seconds
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const CoreConcept = ({image, description, title}) => {
+  return (
+    <li>
+      <img src={image} alt={description} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+
+
+const Header = () => {
+
+  const [word, setWord] = useState(options[0])
+
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+
+      setWord(options[generationNumber(2)])
+    }, 2000)
+
+    return () => clearInterval(interval)
+
+  }, [])
+
+  return (  
+    <header>
+      <img src={ heroImage } alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        { word } React concepts you will need for almost any app you are going
+        to build!
+      </p>
+    </header>
+  )
+};
+
+
 function App() {
   return (
+    <>
+    <Header />
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id = "core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept  
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}            
+          />
+          <CoreConcept
+            {...CORE_CONCEPTS[1]}
+          />
+          <CoreConcept
+            {...CORE_CONCEPTS[2]}
+          />
+          <CoreConcept
+            {...CORE_CONCEPTS[3]}
+          />
+        </ul>
+        </section>
       </main>
     </div>
+    </>
   );
 }
 
